7c2dde0f67169d8d360fd3b33d43468298267b8c
TrayEli <wisdom.eli7@gmail.com>
2024-01-23 06:04:10 -0800

    python classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-# 1-square.py
+# 0-square.py
+
+"""Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-
-    def __init__(self, size):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        self.__size = size
+    pass
